57571761661221b6161bc29df73a7f21eeeb116b
Weefa <Vermolius@gmail.com>
2014-01-08 08:39:15 -0600

    Update intellect.js
    Proposed names for intellect cards.

--- data/effects/intellect.js
+++ data/effects/intellect.js
@@ -1,91 +1,91 @@
 MIND.effect.intellect = {
     "Break Point Hand Limit": {
-        "name": "Break Point Hand Limit",
+        "name": "Defensive Mentality",
         "cost": 3,
         "effect": "Continuous: Double your own Break Point and reduce your own hand limit to 3 cards."
     },
     "Counter Brace": {
-        "name": "Counter Brace",
+        "name": "Ward",
         "cost": 3,
         "effect": "Temporary: Increase target Thinker's Break Point by the total Power of a target card."
     },
     "Continuous Overload": {
-        "name": "Continuous Overload",
+        "name": "Presence",
         "cost": 2,
         "effect": "Continuous: Increase the Cost of all opposing Thoughts by 1."
     },
     "Disengage": {
-        "name": "Disengage",
+        "name": "Prepare",
         "cost": 3,
         "effect": "Immediate: Disengage a target Thought or Group."
     },
     "Double Break Point": {
-        "name": "Double Break Point",
+        "name": "Guard",
         "cost": 4,
         "effect": "Temporary: Double a target Thinker's Break Point."
     },
     "End-turn Disengage": {
-        "name": "End-turn Disengage",
+        "name": "Efficiency",
         "cost": 3,
         "effect": "Continuous: Disengage target Thought at the end of a turn. If target is removed, remove this card."
     },
     "Hand Decoy": {
-        "name": "Hand Decoy",
+        "name": "Mental Barrier",
         "cost": 3,
         "effect": "Immediate: Choose a card from your hand when a card would be removed and discard it instead."
     },
     "Misdirection": {
-        "name": "Misdirection",
+        "name": "Redirect",
         "cost": 2,
         "effect": "Immediate: Change the target of an engagement to a card you control."
     },
     "Negate Effect": {
-        "name": "Negate Effect",
+        "name": "Nullify Effect",
         "cost": 2,
         "effect": "Temporary: Negate the effect of a target card in play."
     },
     "Negate Intellect": {
-        "name": "Negate Intellect",
+        "name": "Nullify Intellect",
         "cost": 3,
         "effect": "Immediate: Negate the effect of an Intellect in play and remove it."
     },
     "Power-amp IV": {
-        "name": "Power-amp IV",
+        "name": "Intensify IV",
         "cost": 1,
         "effect": "Temporary: Increase the Power of all target Thinker's Thoughts by 4."
     },
     "Power-amp X": {
-        "name": "Power-amp X",
+        "name": "Intensify X",
         "cost": 4,
         "effect": "Temporary: Increase the Power of all target Thinker's Thoughts by 10."
     },
     "Power-down": {
-        "name": "Power-down",
+        "name": "Exhaust",
         "cost": 2,
         "effect": "Temporary: Halve the Power of a target card or Group."
     },
     "Power-drain": {
-        "name": "Power-drain",
+        "name": "Debilitate",
         "cost": 4,
         "effect": "Temporary: Reduce the Power of a target card to 0."
     },
     "Recall": {
-        "name": "Recall",
+        "name": "Abstraction",
         "cost": 2,
         "effect": "Immediate: Return one target card in play back to the top of its respective deck."
     },
     "Focus Recovery": {
-        "name": "Focus Recovery",
+        "name": "Disbelief",
         "cost": 8,
-        "effect": "Immediate: Gain 1 Focus Counter. This card can only be played as a response to loosing a Focus Counter."
+        "effect": "Immediate: Gain 1 Focus Counter. This card can only be played as a response to losing a Focus Counter."
     },
     "Memory Loss": {
-        "name": "Memory Loss",
+        "name": "Dismiss",
         "cost": 2,
         "effect": "Immediate: Choose three cards from your deck, discard them, and shuffle your deck."
     },
     "Thought Cannon": {
-        "name": "Thought Cannon",
+        "name": "Thought Bomb",
         "cost": 3,
         "effect": "Immediate: Choose a Thought or Group you control and remove it from play, engage target Thinker with a power equal to the Total Power of the chosen Thought or Group."
     },
@@ -95,47 +95,47 @@ MIND.effect.intellect = {
         "effect": "Immediate: Play any card from your discard pile."
     },
     "Thought Tribute": {
-        "name": "Thought Tribute",
+        "name": "Contort",
         "cost": 4,
         "effect": "Immediate: Remove a Thought you control and play any card from your discard pile with less cost."
     },
     "Thought Recollection": {
-        "name": "Thought Recollection",
+        "name": "Instigate",
         "cost": 4,
         "effect": "Immediate: Choose a Thought from a Thinker's discard pile and play it under that Thinker's control."
     },
     "Intellect Break": {
-        "name": "Intellect Break",
+        "name": "Muddle",
         "cost": 2,
         "effect": "Immediate: Negate and remove a single target Thinker's Intellect in play; target Thinker may discard the top 3 cards of their deck to negate this card."
     },
     "Thought Break": {
-        "name": "Thought Break",
+        "name": "Disturb",
         "cost": 2,
         "effect": "Immediate: Remove a single target Thinker's Thought in play; target Thinker may discard the top 3 cards of their deck to negate this card."
     },
     "Engagement Break": {
-        "name": "Engagement Break",
+        "name": "Disregard",
         "cost": 2,
         "effect": "Immediate: Negate the resolution of an engagement; any Thinker may discard the top 3 cards of their deck to negate this card."
     },
     "Ready Break": {
-        "name": "Ready Break",
+        "name": "Preoccupy",
         "cost": 2,
         "effect": "Immediate: Engage a single target Thinker's Thought or Group in play; target Thinker may discard the top 3 cards of their deck to negate this card."
     },
     "Reload Consciousness": {
-        "name": "Reload Consciousness",
+        "name": "Delusions",
         "cost": 4,
         "effect": "Immediate: Discard the hand of all Thinkers. All Thinkers then draw the same number of cards they discarded from their deck."
     },
     "Amnesia Blast": {
-        "name": "Amnesia Blast",
+        "name": "Compunction",
         "cost": 8,
         "effect": "Immediate: Discard the hand of target Thinker whose discard pile contains 15 or more cards."
     },
     "Removal Amnesia": {
-        "name": "Removal Amnesia",
+        "name": "Branching Amnesiac",
         "cost": 4,
         "effect": "Temporary: Discard the top card of target Thinker's deck when a card they own is removed from play."
     },
@@ -175,12 +175,12 @@ MIND.effect.intellect = {
         "effect": "Immediate: Remove a marked Thought or Intellect in play."
     },
     "Negate Marked": {
-        "name": "Negate Marked",
+        "name": "Nullify Marked",
         "cost": 2,
         "effect": "Temporary: Negate the effect of all marked cards."
     },
     "Power-down Marked": {
-        "name": "Power-down Marked",
+        "name": "Exhaust Marked",
         "cost": 2,
         "effect": "Temporary: Halve the power of all marked Thoughts."
     },
@@ -190,67 +190,67 @@ MIND.effect.intellect = {
         "effect": "Immediate: Group target Thinker's Thoughts that are marked and engage them."
     },
     "Halve Break": {
-        "name": "Halve Break",
+        "name": "Wane",
         "cost": 3,
         "effect": "Temporary: Lower target Thinker's Break point by half."
     },
     "Foreboding Intellect Negate": {
-        "name": "Foreboding Intellect Negate",
+        "name": "Stymie",
         "cost": 1,
         "effect": "Immediate: Discard the top 3 cards of your deck to negate and remove an intellect played."
     },
     "Continuous Misdirection": {
-        "name": "Continuous Misdirection",
+        "name": "Facades",
         "cost": 7,
         "effect": "Continuous: Change the target of an engagement to a card you control."
     },
     "Force Thought Group": {
-        "name": "Force Thought Group",
+        "name": "Corral",
         "cost": 3,
         "effect": "Temporary: Group all of target Thinker's disengaged Thoughts in play. You may only declare a single engagement this turn."
     },
     "Continuous Power XL": {
-        "name": "Continuous Power XL",
+        "name": "Accentuator XL",
         "cost": 3,
         "effect": "Continuous: Increase the power of the target Thought by 40."
     },
     "Continuous Power XX": {
-        "name": "Continuous Power XX",
+        "name": "Accentuator XX",
         "cost": 2,
         "effect": "Continuous: Increase the power of the target Thought by 20."
     },
     "Continuous Discard": {
-        "name": "Continuous Discard",
+        "name": "Malady",
         "cost": 2,
         "effect": "Continuous: Discard the top card of target Thinker's deck when the target Thought resolves an engagement."
     },
     "Continuous Direct Power-up": {
-        "name": "Continuous Direct Power-up",
+        "name": "Champion",
         "cost": 2,
         "effect": "Continuous: Double the power of the target Thought when it challenges a thinker directly."
     },
     "Continuous Disengage": {
-        "name": "Continuous Disengage",
+        "name": "Efficiency",
         "cost": 2,
         "effect": "Continuous: Disengage the target Thought or Group when a turn ends."
     },
     "Foreboding Power": {
-        "name": "Foreboding Power",
+        "name": "Catalyze",
         "cost": 3,
         "effect": "Temporary: Discard any number of cards from the top of your deck to increase the power of a target Thought by 10 for each discarded card."
     },
     "Thought Aggro": {
-        "name": "Thought Aggro",
+        "name": "Bait",
         "cost": 2,
         "effect": "Temporary: Choose one of your own Thoughts in play, all engagements must target that Thought."
     },
     "Tireless Purge": {
-        "name": "Tireless Purge",
+        "name": "Enthusiasm",
         "cost": 4,
         "effect": "Temporary: Disengage the target card if it resolves an engagement and removes a card."
     },
     "Low-cost Block": {
-        "name": "Low-cost Block",
+        "name": "Inhibit II",
         "cost": 3,
         "effect": "Temporary: Thoughts with a cost of 2 or less may not declare an engagement."
     }
